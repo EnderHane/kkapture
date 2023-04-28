84d5a12f21401c08fe12bdf06f2dc4556414c843
EnderHane <53217585+EnderHane@users.noreply.github.com>
2023-04-28 17:45:01 +0800

    chore: adjust building flow

--- InstallRelease.ps1
+++ InstallRelease.ps1
@@ -1,8 +1,10 @@
 $script_dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 New-Item -Path (Join-Path -Path $script_dir -ChildPath "install") -ItemType Directory -Force
 New-Item -Path (Join-Path -Path $script_dir -ChildPath "install\tools") -ItemType Directory -Force
-Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "build\bin\Win32\Release\*") -Destination (Join-Path -Path $script_dir -ChildPath "install") -Force -Recurse
-Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "build\bin\x64\Release\*") -Destination (Join-Path -Path $script_dir -ChildPath "install") -Force -Recurse
+Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "build\bin\Win32\Release\*.exe") -Destination (Join-Path -Path $script_dir -ChildPath "install") -Force -Recurse
+Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "build\bin\Win32\Release\*.dll") -Destination (Join-Path -Path $script_dir -ChildPath "install") -Force -Recurse
+Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "build\bin\x64\Release\*.exe") -Destination (Join-Path -Path $script_dir -ChildPath "install") -Force -Recurse
+Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "build\bin\x64\Release\*.dll") -Destination (Join-Path -Path $script_dir -ChildPath "install") -Force -Recurse
 Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "*") -Include *.txt -Destination (Join-Path -Path $script_dir -ChildPath "install") -Force -Recurse
 Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "*") -Include *.md -Destination (Join-Path -Path $script_dir -ChildPath "install") -Force -Recurse
 Copy-Item -Path (Join-Path -Path $script_dir -ChildPath "tools\*") -Exclude *.zip -Destination (Join-Path -Path $script_dir -ChildPath "install\tools") -Force -Recurse
